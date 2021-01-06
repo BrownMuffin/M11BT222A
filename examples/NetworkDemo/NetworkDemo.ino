7d736e03fc6b27355c83b9ec46cbf6b51427863b
Marvin Fredriksz <mfredriksz@gmail.com>
2021-01-06 21:25:15 +0100

    Add some comments to the network demo

--- examples/NetworkDemo/NetworkDemo.ino
+++ examples/NetworkDemo/NetworkDemo.ino
@@ -19,61 +19,80 @@ void loop() {
   // Single dot
   for (unsigned char i = 0; i < 5; i++)
   {
+    // Turn on dot with index 0
     screen.toggleNetworkDot(0, true);
+    // Draw the dots on the display
     screen.updateNetworkDots();
     delay(100);
+    
+    // Turn off dot with index 0
     screen.toggleNetworkDot(0, false);
+    // Turn on dot with index 1
     screen.toggleNetworkDot(1, true);
+    // Draw the dots on the display
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(1, false);
     screen.toggleNetworkDot(2, true);
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(2, false); 
     screen.toggleNetworkDot(3, true);
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(3, false); 
     screen.toggleNetworkDot(4, true);
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(4, false); 
     screen.toggleNetworkDot(5, true);
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(5, false); 
     screen.toggleNetworkDot(6, true);
     screen.updateNetworkDots();
     delay(300);
+    
     screen.toggleNetworkDot(6, false); 
     screen.toggleNetworkDot(7, true);
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(7, false); 
     screen.toggleNetworkDot(8, true);
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(8, false); 
     screen.toggleNetworkDot(9, true);
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(9, false); 
     screen.toggleNetworkDot(10, true);
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(10, false); 
     screen.toggleNetworkDot(11, true);
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(11, false); 
     screen.toggleNetworkDot(12, true);
     screen.updateNetworkDots();
     delay(100);
+    
     screen.toggleNetworkDot(12, false); 
     screen.toggleNetworkDot(13, true);
     screen.updateNetworkDots();
     delay(300);
+    
     screen.toggleNetworkDot(13, false); 
     screen.updateNetworkDots();
   }
@@ -85,13 +104,18 @@ void loop() {
   for (unsigned char i = 0; i < 5; i++)
   {
     for (unsigned char j = 0; j < 7; j++)
-	  {
-  	  screen.clearNetworkDots();
-  	  screen.toggleNetworkDot(j, true);
-  	  screen.toggleNetworkDot(j + 7, true);
-  	  screen.updateNetworkDots();
-  	  delay(100);
-	  }
+    {
+      // Turn off all the network dots
+      screen.clearNetworkDots();
+
+      // Enable dots from the top and bottom half
+      screen.toggleNetworkDot(j, true);
+      screen.toggleNetworkDot(j + 7, true);
+
+      // Draw the dots on the display
+      screen.updateNetworkDots();
+      delay(100);
+    }
     delay(200);
   }
 
